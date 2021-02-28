ea8545e91592ee1b0f78b1bf64389336c6d94bdd
Jay <>
2021-02-28 14:17:17 +0300

    challenge 9, fix class name

--- app.js
+++ app.js
@@ -48,7 +48,8 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  console.log(req.body.postInput);
+  console.log(req.body.postTitle + ":");
+  console.log(req.body.postBody);
 });
 ////////////////////////////////////////////////////////////////////////////////
 
