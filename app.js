7b1310d9875602267e7bbf1c2cf5fd095c223d42
Jay <>
2021-02-28 13:54:13 +0300

    challenge 8, send input to console

--- app.js
+++ app.js
@@ -46,6 +46,10 @@ app.get("/post", function(req, res) {
 app.get("/compose", function(req, res) {
   res.render('compose');
 });
+
+app.post("/compose", function(req, res) {
+  console.log(req.body.post);
+});
 ////////////////////////////////////////////////////////////////////////////////
 
 
